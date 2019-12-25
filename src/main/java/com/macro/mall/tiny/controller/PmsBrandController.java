5fcd5d1a14d29e135c35619992c007953d0031d7
lc00734 <964758948@qq.com>
2019-12-25 17:33:07 +0800

    11

--- src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
+++ src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
@@ -102,7 +102,7 @@ public class PmsBrandController {
         return CommonResult.success(brandService.getBrand(id));
     }
 
-    @ApiOperation("根据id查询不全的信息bilibili")
+    @ApiOperation("根据id查询不全的信息欧拉欧拉欧拉欧拉")
     @GetMapping("/getMore/id")
     @ResponseBody
     public CommonResult<PmsBrand> getMore(@RequestParam("id") Long id){
--- src/main/resources/mall-tiny-jenkins.sh
+++ src/main/resources/mall-tiny-jenkins.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-app_name='mall-tiny/mall-tiny-jenkins'
+app_name='mall-tiny-jenkins'
+this_name='mall-tiny/mall-tiny-jenkins:1.0-SNAPSHOT'
 docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
+docker image rm -f ${this_name}
+echo '----image rm -f container----'
 docker run -p 8088:8088 --name ${app_name} \
 --link mysql:db \
 -v /etc/localtime:/etc/localtime \
