5df429c572adc9adaaabcc98947c523eb737a156
lc00734 <964758948@qq.com>
2019-12-25 17:34:21 +0800

    11

--- src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
+++ src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
@@ -102,7 +102,7 @@ public class PmsBrandController {
         return CommonResult.success(brandService.getBrand(id));
     }
 
-    @ApiOperation("根据id查询不全的信息欧拉欧拉欧拉欧拉")
+    @ApiOperation("根据id查询不全的信息欧拉欧拉欧拉欧拉yeyeye")
     @GetMapping("/getMore/id")
     @ResponseBody
     public CommonResult<PmsBrand> getMore(@RequestParam("id") Long id){
